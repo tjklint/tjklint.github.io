6cb67086953e4533cd70555c54afa88430e0bf5e
TJ Klint <timothyjklint@gmail.com>
2024-07-18 15:48:43 -0400

    revert: reverted a few problematic commits.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+// @ts-ignore
 import Header from './components/header/header.tsx';
+// @ts-ignore
 import Hero from './components/hero/hero.tsx';
 
 const App: React.FC = () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+// @ts-ignore
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
 
