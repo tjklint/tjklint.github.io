efe77b77ae305369c5ec7f9a0171c679f30fe6dc
TJ Klint <timothyjklint@gmail.com>
2024-07-18 18:15:58 -0400

    feat: add social links underneath the hero.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
 // @ts-ignore
 import Header from './components/header/header.tsx';
 // @ts-ignore
 import Hero from './components/hero/hero.tsx';
+// @ts-ignore
+import SocialLinks from './components/social_links/social_links.tsx';
+
+const AppContainer = styled.div`
+  background-color: #1e1e1e; /* Match the background color of the hero section */
+  min-height: 100vh;
+  padding: 20px 0;
+`;
 
 const App: React.FC = () => {
   return (
     <div>
-      <Header />
-      <Hero />
-      {/* Other components go here */}
+        <AppContainer>
+        <Header />
+        <Hero />
+        <SocialLinks />
+        {/* Other components go here */}
+      </AppContainer>
     </div>
   );
 }
