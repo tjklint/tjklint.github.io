f639c21e775b593fe7b40a8908437b95e1293b1a
TJ Klint <timothyjklint@gmail.com>
2024-07-18 15:13:45 -0400

    revert: changed index.tsx to old configuration.

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container!);
-
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <App />
