cceaee7c345f33b680f954c84fd6f16c9f874792
TJ Klint <timothyjklint@gmail.com>
2024-07-20 00:51:00 -0400

    feat: added responsiveness to the about page.

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -115,23 +115,6 @@ const About: React.FC = () => {
           </div>
         </div>
       </section>
-{/*
-      <section className="about-contact">
-        <h2>Contact Me</h2>
-        <form>
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" required />
-
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" required />
-
-          <label htmlFor="message">Message</label>
-          <textarea id="message" name="message" required></textarea>
-
-          <button type="submit">Send</button>
-        </form>
-      </section>
-*/}
     </div>
   );
 }
