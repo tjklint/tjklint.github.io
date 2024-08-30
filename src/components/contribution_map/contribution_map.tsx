792d0addb15fc68a0e724a401be562c9a3f991dc
TJ Klint <timothyjklint@gmail.com>
2024-08-30 02:28:04 -0400

    style: updated legend and totalCount

--- src/components/contribution_map/contribution_map.tsx
+++ src/components/contribution_map/contribution_map.tsx
@@ -11,10 +11,10 @@ const ContributionMap: React.FC = () => {
   const labels = {
     months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
     weekdays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
-    totalCount: '{{count}} in {{year}}', 
+    totalCount: '{{count}} contributions in {{year}}', 
     legend: {
-      less: 'Weniger',
-      more: 'Mehr',
+      less: 'less',
+      more: 'more',
     },
   };
 
