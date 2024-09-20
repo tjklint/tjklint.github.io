753eeab233a07960a7c4cba0b7b5d6062428161c
TJ Klint <timothyjklint@gmail.com>
2024-09-20 13:43:58 -0400

    feat: added smoothing scrolling and ids to the header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,37 +1,51 @@
 import React, { useState } from 'react';
 import { FaHome, FaUser, FaCode, FaFileAlt, FaBars, FaTimes, FaGithub, FaStar, FaCodeBranch } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 import './header.scss';
 
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>, target: string) => {
+    e.preventDefault();
+    const section = document.getElementById(target);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+    setIsOpen(false);
+  };
+
   return (
     <header className="header-container">
-      <Link to="/" className="logo" onClick={() => setIsOpen(false)}>
+      <a href="#home" className="logo" onClick={(e) => handleClick(e, 'home')}>
         TJ KLINT
-      </Link>
+      </a>
       <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? <FaTimes size={30} className="close-icon" /> : <FaBars size={30} />}
       </div>
       <nav className={`nav ${isOpen ? 'open' : ''}`}>
-        <Link className="nav-link" to="/" onClick={() => setIsOpen(false)}>
+        <a href="#home" className="nav-link" onClick={(e) => handleClick(e, 'home')}>
           <FaHome />
           Home
-        </Link>
-        <Link className="nav-link" to="/about" onClick={() => setIsOpen(false)}>
+        </a>
+        <a href="#about" className="nav-link" onClick={(e) => handleClick(e, 'about')}>
           <FaUser />
           About
-        </Link>
-        <Link className="nav-link" to="/projects" onClick={() => setIsOpen(false)}>
+        </a>
+        <a href="#projects" className="nav-link" onClick={(e) => handleClick(e, 'projects')}>
           <FaCode />
           Projects
-        </Link>
-        <Link className="nav-link" to="/resume" onClick={() => setIsOpen(false)}>
+        </a>
+        <a href="#resume" className="nav-link" onClick={(e) => handleClick(e, 'resume')}>
           <FaFileAlt />
           Resume
-        </Link>
-        <a className="button" href="https://github.com/tjklint/tjklint.github.io" target="_blank" rel="noopener noreferrer" onClick={() => setIsOpen(false)}>
+        </a>
+        <a
+          className="button"
+          href="https://github.com/tjklint/tjklint.github.io"
+          target="_blank"
+          rel="noopener noreferrer"
+          onClick={() => setIsOpen(false)}
+        >
           <FaStar />
           {`or\u00A0`}
           <FaCodeBranch />
